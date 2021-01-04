5104b2216afd9f21142c6ea09c93654237908d55
Keiji Yamaki <k.yamaki@hiroshimamm.com>
2021-01-04 15:59:17 +0900

    Initial Commit

---
+++ Sample3-Walking/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sample3-Walking
+//
+//  Created by keiji yamaki on 2021/01/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sample3-Walking/Sample3_WalkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample3_WalkingApp.swift
+//  Sample3-Walking
+//
+//  Created by keiji yamaki on 2021/01/04.
+//
+
+import SwiftUI
+
+@main
+struct Sample3_WalkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
