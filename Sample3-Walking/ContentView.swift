c6f95de689339471cadb11904566d2f8b8dcad2b
Keiji Yamaki <k.yamaki@hiroshimamm.com>
2021-01-05 11:12:00 +0900

    SwiftUIで、歩数と歩行距離を表示

--- Sample3-Walking/ContentView.swift
+++ Sample3-Walking/ContentView.swift
@@ -6,11 +6,93 @@
 //
 
 import SwiftUI
+import CoreMotion
+
+class MyPedometer: NSObject, ObservableObject {
+    
+    @Published var isStarted = false
+    @Published var isWalking = false
+    @Published var count = 0
+    @Published var distance = 0.0
+    
+    let pedometer = CMPedometer()
+
+    func start() {
+        
+        guard !isStarted else {
+            return
+        }
+        isStarted = true
+            
+        pedometer.startEventUpdates { (event, error) in
+            guard error == nil else {
+                print("error: \(String(describing: error))")
+                return
+            }
+            
+            DispatchQueue.main.async {
+                if event!.type == CMPedometerEventType.pause {
+                    self.isWalking = false
+                } else {
+                    self.isWalking = true
+                }
+            }
+        }
+        
+        pedometer.startUpdates(from: Date()) { (data, error) in
+            guard error == nil else {
+                print("error \(String(describing: error))")
+                return
+            }
+            
+            DispatchQueue.main.async {
+                self.count = data?.numberOfSteps as! Int
+                self.distance = data?.distance as! Double
+            }
+        }
+        
+    }
+    
+    func stop() {
+        
+        guard isStarted else {
+            return
+        }
+        
+        isStarted = false
+        
+        pedometer.stopUpdates()
+        pedometer.stopEventUpdates()
+    }
+    
+}
 
 struct ContentView: View {
+    @ObservedObject var pedometer = MyPedometer()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            if self.pedometer.isWalking {
+                Text("WALKING")
+            } else {
+                Text("STOPPED")
+            }
+            Text("\(self.pedometer.count)")
+            Text("\(self.pedometer.distance)")
+            Button(action: {
+                if !self.pedometer.isStarted {
+                    self.pedometer.start()
+                } else {
+                    self.pedometer.stop()
+                }
+            }) {
+                if !self.pedometer.isStarted {
+                    Text("スタート")
+                } else {
+                    Text("ストップ")
+                }
+            }
+        }
     }
 }
 
